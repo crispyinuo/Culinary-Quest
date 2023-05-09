6639dd3f80f3f168a3e12815606bb631c8c2d893
Zoe Zhou <zoez@usc.edu>
2023-05-08 21:07:26 -0700

    fix login error message

--- login.php
+++ login.php
@@ -1,3 +1,53 @@
+<?php
+session_start();
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+  $servername = "303.itpwebdev.com";
+  $username = "zoez_db_user";
+  $password = "uscitp2023";
+  $dbname = "zoez_recipes";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $sql = "SELECT * FROM users WHERE username = ?";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("s", $username);
+  $stmt->execute();
+  $result = $stmt->get_result();
+
+  if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    if (hash('sha256', $password) === $row['password_hash']) {
+      $_SESSION['user_id'] = $row['user_id'];
+      $_SESSION['username'] = $row['username'];
+      $_SESSION['is_admin'] = $row['is_admin'];
+
+      header("Location: homepage.html");
+      exit();
+    } else {
+      $_SESSION['error'] = "Invalid password";
+      header("Location: login.php");
+      exit();
+    }
+  } else {
+    $_SESSION['error'] = "User not found";
+    header("Location: login.php");
+    exit();
+  }
+
+  $conn->close();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -34,8 +84,9 @@
           <!-- ./ Heading div-->
 
           <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+            <div id="error-msg" class="alert alert-danger" role="alert" style="display:none;"></div>
             <div class="login-form">
-              <form action="login_handler.php" method="POST">
+              <form action="login.php" method="POST">
                 <div class="form-group">
                   <label for="username">Username</label>
                   <input type="text" class="form-control" id="username" name="username" placeholder="Enter username">
@@ -49,8 +100,6 @@
                   Don't have an account?<a href="register.html" class="btn btn-link">Register</a>
                 </p>
               </form>
-
-
             </div>
           </div>
           <!-- ./ Content div-->
@@ -59,6 +108,13 @@
     </section>
     <!-- END Login SECTION -->
   </main>
+  <script>
+    <?php if (isset($_SESSION['error'])): ?>
+      document.getElementById('error-msg').style.display = 'block';
+      document.getElementById('error-msg').innerHTML = '<?php echo $_SESSION['error']; ?>';
+      <?php unset($_SESSION['error']); ?>
+    <?php endif; ?>
+  </script>
 </body>
 
 </html>
